62ae2835f871b1bc0fe998e5382a13ac2cb11e93
Gabriela Campos <gabriela@Gabrielas-MacBook-Pro-2.local>
2021-02-25 12:12:45 -0600

    new solutions

--- lucky-nums/app.py
+++ lucky-nums/app.py
@@ -35,6 +35,35 @@ def create_user():
             "year": year
         }
     }
+########### SOLUTION ################################
+# def get_fact_for_num(num, type="trivia"):
+#     """Given a number, get a fact about it from numberapi."""
+
+#     resp = requests.get(f"http://numbersapi.com/{num}/{type}")
+#     return resp.text
+
+
+# @app.route("/api/get-lucky-num", methods=["POST"])
+# def get_lucky_num():
+#     """Get lucky num, validating input & returning info about num."""
+
+#     received = request.json
+
+#     form = LuckyNumberRequestForm(csrf_enabled=False, data=received)
+
+#     if form.validate_on_submit():
+#         num = randint(1, 100)
+#         year = received['year']
+
+#         return jsonify(
+#             num={"num": num,
+#                  "fact": get_fact_for_num(num)},
+#             year={"year": year,
+#                   "fact": get_fact_for_num(year, type="year")},
+#         )
+
+#     else:
+#         return jsonify(errors=form.errors)
 
 ########### ANOTHER SOLUTION ########################
 # @app.route('/api/get-lucky-num', methods=["POST"])
